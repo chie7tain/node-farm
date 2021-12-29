aec49223f3f83ce43313a9e009eb8d7da7a56aea
chei7tain <fredrickokwuobi@gmail.com>
2021-12-29 17:47:55 +0100

    edited the product html page to prepare it for dynamic data insertion

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const url = require("url");
+const fs = require("fs");
 
-const server = require("http")
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+
+require("http")
   .createServer((req, res) => {
-    console.log(req.url);
-    res.end("hellow from node farm");
+    const pathName = req.url;
+
+    if (pathName === "/overview") {
+      res.end("over page");
+    } else if (pathName === "/api") {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(data);
+    } else {
+      res.writeHead(404, {
+        "Content-type": "text/html",
+        "my-own-header": "hello-world",
+      });
+      res.end("not found");
+    }
   })
   .listen(3000, "127.0.0.1", () => console.log("listening on port 3000"));
