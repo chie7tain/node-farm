dfab6154cf04c6e5fda2d3fe9833ea1d9a232d4a
chei7tain <fredrickokwuobi@gmail.com>
2021-12-29 19:15:13 +0100

    add comments describing routes in index.js and read template files and stored them in variables

--- index.js
+++ index.js
@@ -2,16 +2,29 @@ const url = require("url");
 const fs = require("fs");
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, "utf-8");
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, "utf-8");
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, "utf-8");
 
 require("http")
   .createServer((req, res) => {
     const pathName = req.url;
+    // Overview:
 
-    if (pathName === "/overview") {
+    if (pathName === "/" || pathName === "/overview") {
+
       res.end("over page");
+
+      // Product:
+    } else if (pathName === "/product") {
+      res.end("product page");
+
+      // API:
     } else if (pathName === "/api") {
       res.writeHead(200, { "Content-Type": "application/json" });
       res.end(data);
+
+      // Not found:
     } else {
       res.writeHead(404, {
         "Content-type": "text/html",
