1d2317ab9cb5b4b0e27fa04566d2bb0df199be5b
chei7tain <fredrickokwuobi@gmail.com>
2021-12-29 21:52:55 +0100

    created replaceTemplate module and imported it

--- index.js
+++ index.js
@@ -1,20 +1,6 @@
 const url = require("url");
 const fs = require("fs");
-
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-  if (!product.organic) {
-    output = output.replace(`{%NOT_ORGANIC%}`, `not-organic`);
-  }
-  return output;
-};
+const replaceTemplate = require("./module/replaceTemplate");
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
@@ -53,9 +39,9 @@ require("http")
       // Product:
     } else if (pathName === "/product") {
       const product = dataObj[query.id];
-      console.log(product);
+
       const productTemplate = replaceTemplate(tempProduct, product);
-      console.log(productTemplate);
+
       res.writeHead(200, { "Content-type": "text/html" });
       res.end(productTemplate);
       // API:
